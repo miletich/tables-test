98ec787b33e26482384147b28ba5e23021f3a454
dule <d@miletich.cc>
2018-03-15 20:12:17 +0100

    Enable sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import moment from 'moment';
 
-const formatTitle = string =>
-  `${string.charAt(0).toUpperCase()}${string.slice(1)}`.replace('_', ' ');
+import TableHeading from './TableHeading';
+
+const sortByKey = key => (a, b) => {
+  if (typeof a[key] === 'string') {
+    return a[key].localeCompare(b[key]);
+  } else if (typeof a[key] === 'object') {
+    return (
+      moment(`${b[key].date} ${b[key].time}`).valueOf() -
+      moment(`${a[key].date} ${a[key].time}`).valueOf()
+    );
+  }
+  return a[key] - b[key];
+};
 
 class Table extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: this.props.data };
+    this.state = { data: this.props.data, sortBy: null, sortedData: this.props.data };
+    this.handleTitleClick = this.handleTitleClick.bind(this);
   }
+
+  handleTitleClick(sorter) {
+    const { data, sortBy } = this.state;
+    this.setState({
+      sortBy: sortBy === sorter ? null : sorter,
+      sortedData: sortBy === sorter ? data : [...data].sort(sortByKey(sorter)),
+    });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, sortedData } = this.state;
     return (
       <table className="table">
         <thead>
           <tr>
-            {Object.entries(data[0]).map(([key, value]) => {
-              if (typeof value === 'number' || (typeof value === 'string' && value.includes('$'))) {
-                return (
-                  <th key={key} scope="cell" style={{ textAlign: 'right' }}>
-                    {formatTitle(key)}
-                  </th>
-                );
-              }
-              return (
-                <th key={key} scope="cell">
-                  {formatTitle(key)}
-                </th>
-              );
-            })}
+            <TableHeading
+              firstRow={Object.entries(data[0])}
+              onHandleTitleClick={this.handleTitleClick}
+            />
           </tr>
         </thead>
         <tbody>
-          {data.map(item => (
+          {sortedData.map(item => (
             <tr key={item.id}>
               {Object.values(item).map((value) => {
                 if (typeof value === 'object') {
                   return (
                     <td key={value}>
-                      <Moment fromNow>{value}</Moment>
+                      <Moment fromNow>{`${value.date} ${value.time}`}</Moment>
                     </td>
                   );
                 } else if (typeof value === 'string') {
---
+++ src/components/TableHeading.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const formatTitle = string =>
+  `${string.charAt(0).toUpperCase()}${string.slice(1)}`.replace('_', ' ');
+
+class TableHeading extends React.PureComponent {
+  constructor() {
+    super();
+    this.sortValues = this.sortValues.bind(this);
+  }
+
+  sortValues(e) {
+    e.preventDefault();
+    this.props.onHandleTitleClick(e.target.dataset.filter);
+  }
+  render() {
+    const { firstRow } = this.props;
+    return firstRow.map(([key, value]) => {
+      if (typeof value === 'number' || (typeof value === 'string' && value.includes('$'))) {
+        return (
+          <th
+            key={key}
+            scope="cell"
+            style={{ textAlign: 'right' }}
+            data-filter={key}
+            onClick={this.sortValues}
+          >
+            {formatTitle(key)}
+          </th>
+        );
+      }
+      return (
+        <th key={key} scope="cell" data-filter={key} onClick={this.sortValues}>
+          {formatTitle(key)}
+        </th>
+      );
+    });
+  }
+}
+
+TableHeading.propTypes = {
+  firstRow: PropTypes.arrayOf(PropTypes.array).isRequired,
+  onHandleTitleClick: PropTypes.func.isRequired,
+};
+
+export default TableHeading;
